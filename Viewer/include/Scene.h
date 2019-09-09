4b3f6e4eb6fa43e629bee43691d61921e18ecb7f
shahar2018 <44222998+shahar2018@users.noreply.github.com>
2019-09-09 08:50:26 +0300

    Update Scene.h

--- Viewer/include/Scene.h
+++ Viewer/include/Scene.h
@@ -21,27 +21,27 @@ private:
 
 	int activeCameraIndex;
 	int activeModelIndex;
+	int width, height;
 
 public:
 	Scene();
 	~Scene(){}
 	void AddModel(const std::shared_ptr<MeshModel>& model);
 	void addLight(const Light &l) { this->Lights.push_back(l); }
-
-	const int GetModelCount() const;
-
 	void AddCamera(const Camera& camera);
-	const int GetCameraCount() const;
-
-	void SetActiveCameraIndex(int index);
-	const int GetActiveCameraIndex() const;
-
-	void SetActiveModelIndex(int index);
-	const int GetActiveModelIndex() const;
-
-	const std::vector<std::shared_ptr<MeshModel>> getModels() const;
-
-	const std::vector<Camera> getCameras() const;
 	
+	//setters
+	void SetActiveCameraIndex(int index);
+	void SetActiveModelIndex(int index);
+
+	//getters
+	const int getHeight() const { return this->height; }
+	const int getWidth() const { return this->width; }
+	const int GetModelCount() const;
+	const int GetCameraCount() const;
+	const int GetActiveCameraIndex() const;
+	const int GetActiveModelIndex() const;
+	const std::vector<std::shared_ptr<MeshModel>> getModels() const;
+	const std::vector<Camera> getCameras() const;
 	const std::vector<Light> getLights() const { return this->Lights; }
 };
